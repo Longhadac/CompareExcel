9c132a3029f8eaf69810aa6615a0af60449335f6
Long2585 <long2585@live.com>
2018-01-14 21:20:39 +0700

    First commit

---
+++ CompareExcelFiles/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CompareExcelFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.btnFile2 = new System.Windows.Forms.Button();
+            this.txbFile1 = new System.Windows.Forms.TextBox();
+            this.txbFile2 = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFile1
+            // 
+            this.btnFile1.Location = new System.Drawing.Point(439, 10);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(75, 23);
+            this.btnFile1.TabIndex = 0;
+            this.btnFile1.Text = "Open File 1";
+            this.btnFile1.UseVisualStyleBackColor = true;
+            this.btnFile1.Click += new System.EventHandler(this.btnFile1_Click);
+            // 
+            // btnFile2
+            // 
+            this.btnFile2.Location = new System.Drawing.Point(439, 39);
+            this.btnFile2.Name = "btnFile2";
+            this.btnFile2.Size = new System.Drawing.Size(75, 23);
+            this.btnFile2.TabIndex = 1;
+            this.btnFile2.Text = "Open File 2";
+            this.btnFile2.UseVisualStyleBackColor = true;
+            this.btnFile2.Click += new System.EventHandler(this.btnFile2_Click);
+            // 
+            // txbFile1
+            // 
+            this.txbFile1.Location = new System.Drawing.Point(12, 12);
+            this.txbFile1.Name = "txbFile1";
+            this.txbFile1.Size = new System.Drawing.Size(421, 20);
+            this.txbFile1.TabIndex = 2;
+            this.txbFile1.Text = "C:\\Users\\long2\\Desktop\\T10.xlsx";
+            // 
+            // txbFile2
+            // 
+            this.txbFile2.Location = new System.Drawing.Point(12, 38);
+            this.txbFile2.Name = "txbFile2";
+            this.txbFile2.Size = new System.Drawing.Size(421, 20);
+            this.txbFile2.TabIndex = 3;
+            this.txbFile2.Text = "C:\\Users\\long2\\Desktop\\T11.xlsx";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(78, 82);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 4;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 390);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.txbFile2);
+            this.Controls.Add(this.txbFile1);
+            this.Controls.Add(this.btnFile2);
+            this.Controls.Add(this.btnFile1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.Button btnFile2;
+        private System.Windows.Forms.TextBox txbFile1;
+        private System.Windows.Forms.TextBox txbFile2;
+        private System.Windows.Forms.Button btnCompare;
+    }
+}
+
---
+++ CompareExcelFiles/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompareExcelFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            DataTable file1 = ParseExcelFile(txbFile1.Text);
+            DataTable file2 = ParseExcelFile(txbFile2.Text);
+
+            //Delete header rows
+            for(int i=0;i<13;i++)
+            {
+                file1.Rows[i].Delete();
+                file2.Rows[i].Delete();
+            }
+            file1.AcceptChanges();
+            file2.AcceptChanges();            
+            
+            ExportToExcel(ReverseRowsInDataTable(CompareDataTable(file1, file2))
+                , ConfigurationManager.AppSettings.Get("ExportExcelFile"));
+        }
+
+        private DataTable ParseExcelFile(string fileName)
+        {
+            DataTable results = new DataTable();
+            string sheetName = ConfigurationManager.AppSettings["SheetName"];
+
+            try
+            {
+                string connString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0;HDR=no'", fileName);
+                string sql = "SELECT * FROM [" + sheetName.ToString() + "]";
+                using (OleDbConnection conn = new OleDbConnection(connString))
+                {
+                    conn.Open();
+                    using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+                    {
+                        using (OleDbDataReader rdr = cmd.ExecuteReader())
+                        {
+                            results.Load(rdr);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Couldnot parse file");
+            }
+            return results;
+        }
+
+        public DataTable ReverseRowsInDataTable(DataTable inputTable)
+        {
+            DataTable outputTable = inputTable.Clone();
+            for (int i = inputTable.Rows.Count - 1; i >= 0; i--)
+            {
+                outputTable.ImportRow(inputTable.Rows[i]);
+            }
+            return outputTable;
+        }
+
+        private DataTable CompareDataTable(DataTable file1, DataTable file2, 
+            List<string> keys, List<string> compareValues)
+        {
+            DataTable result = file1.Clone();
+            for (int i = file1.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow row1 = file1.Rows[i];
+                DataRow newRow = row1;
+                for (int j = file2.Rows.Count - 1; j >= 0; j--)
+                {
+                    DataRow row2 = file2.Rows[j];
+                    if ((row1[1].ToString() == row2[1].ToString())
+                        && (row1[3].ToString() == row2[3].ToString())
+                        && (row1[4].ToString() == row2[4].ToString())
+                        && (row1[5].ToString() == row2[5].ToString()))
+                    {
+                        for (int k = 6; k < 35; k++)
+                        {
+                            if (string.IsNullOrEmpty(row1[k].ToString()))
+                            {
+                                if (!string.IsNullOrEmpty(row2[k].ToString()))
+                                {
+                                    newRow[k] = "-" + row2[k].ToString();
+                                }
+                            }
+                            else
+                            {
+                                if (!string.IsNullOrEmpty(row2[k].ToString()))
+                                {
+                                    newRow[k] = Double.Parse(row1[k].ToString()) - Double.Parse(row2[k].ToString());
+                                }
+                            }
+                        }
+                        file2.Rows[j].Delete();
+                        file2.AcceptChanges();
+                        break;
+                    }
+                }
+                result.Rows.Add(newRow.ItemArray);
+                file1.Rows[i].Delete();
+                file2.AcceptChanges();
+            }
+            return result;
+        }
+
+        private void ExportToExcel(DataTable DataTable, string ExcelFilePath = null)
+        {
+            try
+            {
+                int ColumnsCount;
+
+                if (DataTable == null || (ColumnsCount = DataTable.Columns.Count) == 0)
+                    throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+                // load excel, and create a new workbook
+                Microsoft.Office.Interop.Excel.Application Excel = new Microsoft.Office.Interop.Excel.Application();
+                Excel.Workbooks.Add();
+
+                // single worksheet
+                Microsoft.Office.Interop.Excel._Worksheet Worksheet = Excel.ActiveSheet;
+
+                object[] Header = new object[ColumnsCount];
+
+                // column headings               
+                for (int i = 0; i < ColumnsCount; i++)
+                    Header[i] = DataTable.Columns[i].ColumnName;
+
+                Microsoft.Office.Interop.Excel.Range HeaderRange = Worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[1, 1]), (Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[1, ColumnsCount]));
+                HeaderRange.Value = Header;
+                HeaderRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.LightGray);
+                HeaderRange.Font.Bold = true;
+
+                // DataCells
+                int RowsCount = DataTable.Rows.Count;
+                object[,] Cells = new object[RowsCount, ColumnsCount];
+
+                for (int j = 0; j < RowsCount; j++)
+                    for (int i = 0; i < ColumnsCount; i++)
+                        Cells[j, i] = DataTable.Rows[j][i];
+
+                Worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[2, 1]), (Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[RowsCount + 1, ColumnsCount])).Value = Cells;
+
+                // check fielpath
+                if (ExcelFilePath != null && ExcelFilePath != "")
+                {
+                    try
+                    {
+                        Worksheet.SaveAs(ExcelFilePath);
+                        Excel.Quit();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("ExportToExcel: Excel file could not be saved! Check filepath.\n"
+                            + ex.Message);
+                    }
+                }
+                else    // no filepath is given
+                {
+                    Excel.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ExportToExcel: \n" + ex.Message);
+            }
+        }
+
+        private void btnFile1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "ExcelFiles (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txbFile1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnFile2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            //openFileDialog1.Filter = "excel files 2003 (*.xls)|*.xls|2007 (*.xlsx)|*.xlsx";
+            openFileDialog1.Filter = "ExcelFiles (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txbFile2.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private static void WriteLog(string logData, bool logTimeStamp = true)
+        {
+            try
+            {
+                using (StreamWriter w = File.AppendText(ConfigurationManager.AppSettings.Get("LogFile")))
+                {
+                    if (logTimeStamp)
+                    {
+                        logData = DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + ": " + logData;
+                    }
+                    w.WriteLine(logData);
+                }
+            }
+            catch
+            { }
+        }
+
+        private List<string> ParseStringList(string list)
+        {
+            return list.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+        }   
+    }
+}
